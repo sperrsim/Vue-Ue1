6cbddf4378b0bf11f461f3021e6bdc6ce04a0eac
sperrsim <72546243+sperrsim@users.noreply.github.com>
2022-10-10 12:19:33 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vue-Ue1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-taarpc)
---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>Moin</h1>
+    <!--<img alt="Vue logo" src="https://vuejs.org/images/logo.png">-->
+    <HelloWorld msg="Hoes an bitches"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
